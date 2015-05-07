67fef73db2f2a9f54471363639ce6f10f557ac55
kpyfan <kpyfan@uw.edu>
2015-05-07 00:03:42 -0700

    Heroku init

---
+++ shared-music/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', function (req, res) {
+        res.render('index');
+});
+
+
+//adding next 3 get() for zero release / product page
+router.get('/zero.html', function (req, res) {
+        res.sendFile(path.join(__dirname, '../views/zero.html'))
+});
+
+router.get('/dev_documentation', function (req, res) {
+	res.sendFile(path.join(__dirname, '../documentation/dev_documentation.pdf'))
+});
+
+router.get('/user_documentation', function(req, res) {
+	res.sendFile(path.join(__dirname, '../documentation/user_documentation.pdf'))
+});
+
+
+module.exports = router;
---
+++ shared-music/test/basicTest.js
@@ -0,0 +1,12 @@
+var should = require('should');
+  
+describe("Basic Test",function(){
+
+  it('This should pass!',function(done){
+    var flag = true;
+
+
+    flag.should.equal(true);
+    done();
+  });
+});
