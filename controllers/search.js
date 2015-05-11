546d1c9a10339aac0948b3b281d63be4dd15d819
keith619 <wildwinds619@gmail.com>
2015-05-11 01:59:33 -0700

    FIlter works properly now

--- controllers/search.js
+++ controllers/search.js
@@ -34,43 +34,42 @@ angular.module('search', [])
             });
         };
 
-        // Applies all the filters to the current search result
+        // Applies all the filters to the current search result and sorts according to duration
         search.filter = function() {
             var temp = [];
             var results = search.results;
 
             for (i = 0; i < results.length; i++) {
-                if ((results[i].title.toLowerCase().indexOf(search.title.toLowerCase()) > -1) &&
-                    (results[i].genre.toLowerCase().indexOf(search.genre.toLowerCase()) > -1) &&
-                    (results[i].user.username.toLowerCase().indexOf(search.user.toLowerCase()) > -1)) {
+                if ((!search.title || results[i].title.toLowerCase().indexOf(search.title.toLowerCase()) > -1) &&
+                    (!search.genre || results[i].genre.toLowerCase().indexOf(search.genre.toLowerCase()) > -1) &&
+                    (!search.user || results[i].user.username.toLowerCase().indexOf(search.user.toLowerCase()) > -1)) {
                     temp.push(results[i]);
                 }
             }
-            temp = sortByDuration(temp);
             search.display = temp;
         };
 
-        var sortByDuration = function(tracks) {
-            return tracks.sort(function(track1, track2) {
-                if (search.ascending) {
-                    if (track1.duration <= track2.duration) {
-                        return -1;
-                    } else {
-                        return 1;
-                    }
+        search.sortByDuration = function() {
+            search.display = search.display.sort(function(track1, track2) {
+                if (track1.duration < track2.duration) {
+                    return search.ascending ? -1 : 1;
+                } else if (track1.duration > track2.duration) {
+                    return search.ascending ? 1 : -1;
                 } else {
-                    if (track1.duration >= track2.duration) {
-                        return -1;
-                    } else {
-                        return 1;
-                    }
+                    return 0;
                 }
             });
+        };
+
+        search.millisToMinutesAndSeconds = function(millis) {
+            var minutes = Math.floor(millis / 60000);
+            var seconds = ((millis % 60000) / 1000).toFixed(0);
+            return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         };
 
         search.addSong = function(n) {
             // call add song function with the given name
             // socket.io?
-            alert('Added song' + search.display[n].title);
+            alert('Added song: ' + search.display[n].title);
         };
     });
