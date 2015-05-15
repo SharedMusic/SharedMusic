8f4ac8e19226e9124aa0f1ce55bb9f3ddf1b69c2
Eric Nhan <Eric@Erics-MacBook-Air.local>
2015-05-14 23:06:22 -0700

    added ability to mute song and handled some cases where the url was incorrect

--- controllers/musicplayer.js
+++ controllers/musicplayer.js
@@ -6,6 +6,9 @@ musicPlayer.controller('MusicPlayer', ['$scope', function($scope){
 	mP.currentSongURL = null;
 	mP.currentSong = null;
 
+	mP.muted = false;
+	mP.muteStatus = "Mute"
+
 	SC.initialize({
 	  client_id: '337bccb696d7b8442deedde76fae5c10'
 	});
@@ -19,13 +22,22 @@ musicPlayer.controller('MusicPlayer', ['$scope', function($scope){
 
 			// fetch track information
 			SC.get("/resolve/?url="+mP.currentSongURL, {limit: 1}, function(result){
-				mP.trackInfo = result;
+				if (result.errors == null) {
+					if (result.kind != "track") {
+						console.log("url is not for a song")
+					} else {
+						mP.trackInfo = result;
 
-				// apply update to the view - needed because this is a callback that excutes after
-				$scope.$apply();
+						// apply update to the view - needed because this is a callback that excutes after
+						$scope.$apply();
 
-				// play the song when results are returned
-				mP.playSong();
+						// play the song when results are returned
+						mP.playSong();
+					}
+				} else {
+					// error is given songURL
+					console.log("error")
+				}
 			});
 
 		}
@@ -44,6 +56,21 @@ musicPlayer.controller('MusicPlayer', ['$scope', function($scope){
 				mP.currentSong = sound;
 				mP.currentSong.play();
 			});
+		}
+	}
+
+	mP.muteSong = function() {
+		if (mP.currentSong != null) {
+			if (mP.muted) {
+				mP.muted = false
+				mP.muteStatus = "Mute"
+				mP.currentSong.unmute()
+			} else {
+				mP.muted = true
+				mP.muteStatus = "Unmute"
+				mP.currentSong.mute()
+			}
+
 		}
 	}
 }]);
