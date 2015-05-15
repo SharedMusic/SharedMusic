37992a649812226fc6211d3d27bf6c5581a25e4f
reggiej7 <reggiejones7@hotmail.com>
2015-05-15 13:53:46 -0700

    updating paths for tiles

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'style'))); 
+app.use(express.static(path.join(__dirname, 'js'))); 
 
 app.use('/', routes);
 app.use('/users', users);
